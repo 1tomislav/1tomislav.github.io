81bf7b14138c7f27ab46af97077e1527a17d0393
1tomislav <1tomislav.petrovic@gmail.com>
2025-05-25 21:44:59 +0200

    Update timepassed.js

--- javascript/timepassed.js
+++ javascript/timepassed.js
@@ -24,6 +24,7 @@ function updateTime() {
         document.getElementById("timePassed").textContent = formatted;
         lastFormatted = formatted;
     }
+    else {}
 }
 
 let lastFormatted = "";
